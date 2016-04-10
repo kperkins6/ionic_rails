4a8866603a6f1002c15c576eaaed2081460db4f0
Kevin Perkins <kperkins6@mail.csuchico.edu>
2016-04-10 15:37:17 -0700

    Created devise sessions controllers

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'user/sessions',
+  registrations: 'user/registrations', passwords: 'user/passwords' }
   resources :decks
   resources :tagcards
   resources :bcards
