baf0af2556419bb83217bee60da9cba3efd15d7c
Kevin Perkins <kperkins6@mail.csuchico.edu>
2016-04-10 17:38:19 -0700

    Still not working

--- mobile/www/js/services.js
+++ mobile/www/js/services.js
@@ -1,16 +1,16 @@
-angular.module('app.services', ['ngResource'])
+angular.module('app.services', [])
 
 .factory('BlankFactory', [function(){
 
 }])
 
-.factory('Bcard', function($resource) {
-  return $resource("http://localhost:3000/bcards/:id.json");
-})
-
-.factory('UserSession', function($resource) {
-  return $resource("http://localhost:3000/users/sign_in.json");
-})
+// .factory('Bcard', function($resource) {
+//   return $resource("http://localhost:3000/bcards/:id.json");
+// })
+//
+// .factory('UserSession', function($resource) {
+//   return $resource("http://localhost:3000/users/sign_in.json");
+// })
 
 .service('BlankService', [function(){
 
