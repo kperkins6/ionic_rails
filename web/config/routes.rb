4b85863309d63414c6cef64e6ad6f51a407e51bf
Kevin Perkins <kperkins6@mail.csuchico.edu>
2016-04-10 20:39:02 -0700

    Save point beforechanging auth service

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {sessions: 'user/sessions',
   registrations: 'user/registrations', passwords: 'user/passwords' }
+  
   resources :decks
   resources :tagcards
   resources :bcards
