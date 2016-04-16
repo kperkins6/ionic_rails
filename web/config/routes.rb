d3e7f851999e8f4eb101cdab0fe047075ea85af5
Kevin Perkins <kperkins6@mail.csuchico.edu>
2016-04-16 16:29:51 -0700

    Deleted index from routes

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {sessions: 'user/sessions', registrations: 'user/registrations', passwords: 'user/passwords' }
-  match "/bcards" => "application#index", via: :options
+  # match "/bcards" => "application#index", via: :options
   # devise_for :users, :controllers => {sessions: 'sessions'}
 
   resources :decks
