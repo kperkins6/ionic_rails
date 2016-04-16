fdb83d96720674561c605230a565ae6a76a3d7c5
Kevin Perkins <kperkins6@mail.csuchico.edu>
2016-04-16 16:33:58 -0700

    Updated Routes

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {sessions: 'user/sessions', registrations: 'user/registrations', passwords: 'user/passwords' }
   # match "/bcards" => "application#index", via: :options
   # devise_for :users, :controllers => {sessions: 'sessions'}
-
+  scope '/bcards' do
+    get '/' => 'bcards_controller#index'
+    post '/' => 'bcards_controller#create'
+    patch '/' => 'bcards_controller#update'
+  end
   resources :decks
   resources :tagcards
   resources :bcards
