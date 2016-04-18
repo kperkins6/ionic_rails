9a8bce34b849574870cf950fbc76b1af1bff4c29
Kevin Perkins <kperkins6@mail.csuchico.edu>
2016-04-17 23:04:35 -0700

    Yup fucked it up

--- web/app/controllers/user/sessions_controller.rb
+++ web/app/controllers/user/sessions_controller.rb
@@ -1,34 +1,59 @@
 class User::SessionsController < Devise::SessionsController
-  prepend_before_filter :require_no_authentication, :only => [:create ]
-  before_filter :ensure_params_exist
+  respond_to :json, :html
 
-  respond_to :json
+  # GET /resource/sign_in
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
+  end
 
+  # POST /resource/sign_in
   def create
-    build_resource
-    resource = User.find_for_database_authentication(:login=>params[:user_login][:login])
-    return invalid_login_attempt unless resource
-
-    if resource.valid_password?(params[:user_login][:password])
-      sign_in("user", resource)
-      render :json=> {:success=>true, :auth_token=>resource.authentication_token, :login=>resource.login, :email=>resource.email}
-      return
-    end
-    invalid_login_attempt
+    sign_out(:user) if current_user
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
   end
 
+  # DELETE /resource/sign_out
   def destroy
-    sign_out(resource_name)
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    respond_to_on_destroy
   end
 
-  protected
-  def ensure_params_exist
-    return unless params[:user_login].blank?
-    render :json=>{:success=>false, :message=>"missing user_login parameter"}, :status=>422
-  end
+  private
 
-  def invalid_login_attempt
-    warden.custom_failure!
-    render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
+# Check if there is no signed in user before doing the sign out.
+#
+# If there is no signed in user, it will set the flash message and redirect
+# to the after_sign_out path.
+def verify_signed_out_user
+  if all_signed_out?
+    set_flash_message! :notice, :already_signed_out
+
+    respond_to_on_destroy
   end
+end
+
+def all_signed_out?
+  users = Devise.mappings.keys.map { |s| warden.user(scope: s, run_callbacks: false) }
+
+  users.all?(&:blank?)
+end
+
+def respond_to_on_destroy
+  # We actually need to hardcode this as Rails default responder doesn't
+  # support returning empty response on GET request
+  respond_to do |format|
+    format.all { head :no_content }
+    format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
+  end
+end
+
 end
